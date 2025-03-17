288adc7af00aed4193e0055e7702ff36cefee3f1
bassey <basseyinameti@gmail.com>
2025-03-17 20:33:20 +0100

    Declaring a variable

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -2,4 +2,19 @@ package org.example
 
 fun main(args: Array<String>){
     println("Hello Kotlin")
+//  Different ways to declare a variable
+    var decimal = 3.40
+    val number: Int = readln().toInt()
+    var a: String = readln()
+
+    val miniTest = Test("miniSo",23)
+    miniTest.name="math Test"
+
+    val maxTest: Test
+
+    println(miniTest)
+}
+
+class Test(var name: String, val id: Int) {
+
 }
